06079b93bb2b678a46bab3d2ac8b21bf7c59c78b
Abhijeet1326 <buntysadiq@gmail.com>
2024-03-10 23:42:13 +0530

    Login and SignUp Update

--- routes/news.js
+++ routes/news.js
@@ -4,6 +4,71 @@ const axios = require('axios')
 const newsr=express.Router()
 const moment = require('moment')
 const math = require('math')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect("mongodb+srv://news-pulse:news-pulse@cluster0.ldez3vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+const userSchema = mongoose.Schema({
+    email: String,
+    phone: String,
+    password: String
+});
+const Item = mongoose.model("USer", userSchema);
+
+newsr.get('/login', (req, res) => {
+    res.render("login.ejs");
+});
+
+newsr.post("/login", async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const check = await Item.findOne({email:email});
+    const passMatch = await bcrypt.compare(password, check.password);
+    console.log(check);
+    if(!check){
+        return res.status(200).json({message: "Cannot Create  New"});
+          
+    }
+    if(passMatch){
+        const show = email.slice(0,1).toUpperCase();
+        const search = req.query.search || "general"
+        const begin = "2024-02-21"
+        const end = "2024-02-26"
+        var url = `https://newsapi.org/v2/everything?q=${search}&from=${begin}&to=${end}&sortBy=popularity&apiKey=8200207d22d148b596df946f1e4ff792`
+        const news_get =await axios.get(url)
+
+        
+        res.render('news',{articles:news_get.data.articles, butt:show});
+    }
+    else{
+        return res.status(300).json({message: "Wrong password New"}); 
+    } 
+});
+
+newsr.get('/signup', async(req, res) => {
+    res.render("signup.ejs",{mess:""});
+});
+
+newsr.post("/signup", async(req, res) => {
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const pass = req.body.password;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(pass, saltRounds);
+    const ext = await Item.findOne({email:email});
+    if(ext){
+        res.render("signup.ejs",{mess:"User Already Exists!!"});
+    }
+    else{
+        await Item.create({
+            email:email,
+            phone:phone,
+            password:hashedPassword
+        });
+        res.render("login.ejs");
+    }
+});
+
 
 
 newsr.get('/',async(req,res)=>{
@@ -15,15 +80,17 @@ newsr.get('/',async(req,res)=>{
         const news_get =await axios.get(url)
 
         
-        res.render('news',{articles:news_get.data.articles})
+        res.render('news',{articles:news_get.data.articles, butt:"Login/SignUp"})
 
     } catch (error) {
         if(error.response){
-            console.log(error)
+            console.log(error);
         }
 
     }
-})
+});
+
+
 
 // newsr.get('/login', (req, res) => {
 //     res.render('login')
@@ -63,7 +130,7 @@ newsr.post('/search',async(req,res)=>{
     try {
         var url = `https://newsapi.org/v2/everything?q=${search}&from=${begin}&to=${end}&sortBy=popularity&apiKey=8200207d22d148b596df946f1e4ff792`
         const news_get =await axios.get(url)
-        res.render('news',{articles:news_get.data.articles})
+        res.render('news',{articles:news_get.data.articles, butt:"Login/SignUp"})
     } catch (error) {
         if(error.response){
             console.log(error)
@@ -78,7 +145,7 @@ newsr.get('/news/:category',async(req,res)=>{
         var url = 'http://newsapi.org/v2/top-headlines?country=in&category=' + category + '&apiKey=36f3e29b704f41339af8439dc1228334';
 
         const news_get =await axios.get(url)
-        res.render('category',{articles:news_get.data.articles})
+        res.render('category',{articles:news_get.data.articles, butt:"Login/SignUp"})
 
     } catch (error) {
         if(error.response){
@@ -88,6 +155,6 @@ newsr.get('/news/:category',async(req,res)=>{
     }
 })
 
-newsr.get
+
 
 module.exports=newsr
